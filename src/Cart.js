4ab1158e07b7543c54990da4c93cc65524a5f049
arpit <arpitrahul02@gmail.com>
2023-01-12 06:42:35 +0530

    add props to cartItem

--- src/Cart.js
+++ src/Cart.js
@@ -2,12 +2,44 @@ import React from 'react';
 import CartItem from './CartItem'
 
 class Cart extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            products: [
+            {
+                price: 99,
+                title: 'Watch',
+                qty: 2,
+                img: '',
+                id: 1
+            },
+            {
+                price: 999,
+                title: 'Phone',
+                qty: 4,
+                img: '',
+                id: 2
+            },
+            {
+                price: 9999,
+                title: 'TV',
+                qty: 1,
+                img: '',
+                id: 3
+            }
+        ]
+    }
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
+        // this.decreaseQuantity = this.decreaseQuantity.bind(this);
+    }
+
     render(){
+        const {products} = this.state;
         return(
             <div className='cart'>
-                <CartItem />
-                <CartItem />
-                <CartItem />
+                {products.map((product) => {
+                    return <CartItem product = {product} key = {product.id} />
+                })}
             </div>
         );
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,17 +1,7 @@
 import React from "react";
 
 class CartItem extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            price: 999,
-            title: 'Phone',
-            qty: 1,
-            img: ''
-        }
-        this.increaseQuantity = this.increaseQuantity.bind(this);
-        this.decreaseQuantity = this.decreaseQuantity.bind(this);
-    }
+
 
     increaseQuantity = () => {
         // setState form 1
@@ -30,7 +20,7 @@ class CartItem extends React.Component{
     decreaseQuantity = () => {
         const {qty} = this.state;
 
-        if(qty == 0){
+        if(qty === 0){
             return;
         }
 
@@ -42,11 +32,11 @@ class CartItem extends React.Component{
     }
 
     render(){
-        const {price, title, qty} = this.state;
+        const {price, title, qty} = this.props.product;
         return(
             <div className="cart-item">
                 <div className="left-block">
-                    <img style={styles.image} />
+                    <img alt="" style={styles.image} />
                 </div>
                 <div className="right-block">
                     <div style={{fontSize: 25}}> {title} </div>
